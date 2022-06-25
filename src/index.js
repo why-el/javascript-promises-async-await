fa85700cb56e38bf34b6e3c98fae65395b165431
Mohamed Wael Khobalatte <wael@vendr.com>
2022-06-25 11:20:08 -0400

    module 3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-export default function fetchMovies() {}
+import { fetchWithTimeout } from './services'
+const movies = require("./data/movies.json")
+
+export default function fetchMovies() {
+  const resolveFunction = () => movies;
+  return fetchWithTimeout(1000).then(resolveFunction);
+}
+
+var moviePromise = fetchMovies()
+
+moviePromise.then(function(results) { console.log(results)})
--- src/services.js
+++ src/services.js
@@ -1,3 +1,3 @@
-export default function fetchWithTimeout(delay) {
+export function fetchWithTimeout(delay) {
   return new Promise(resolve => setTimeout(resolve, delay));
 }
